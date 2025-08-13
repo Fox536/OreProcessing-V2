b63d7d32184ccd69c77f62fae5a402307f69e3ae
Fox536 <34255581+Fox536@users.noreply.github.com>
2025-08-12 22:09:03 -0500

    Add files via upload

---
+++ kubejs/server_scripts/FoxOreProcessing/Ores/_template.js
@@ -0,0 +1,106 @@
+let Fox = Fox || {};
+//----------------------------------------------------------------------------------------------------
+// * Processing
+//----------------------------------------------------------------------------------------------------
+// Namespaces
+Fox.Processing = Fox.Processing || {};
+Fox.Processing.Ores = Fox.Processing.Ores || {};
+Fox.Processing.OresSetup = Fox.Processing.OresSetup || {}
+
+(function() {
+	let namespace = Fox.Processing;
+
+	let oreName 		= '';
+	let isMetal			= true;
+	let breakAmount 	= 3;
+
+	let data 				= {};
+	data.ore				= '#forge:ores/' + oreName;
+	data.raw				= '#forge:raw_materials/' + oreName;
+	data.rawBlock			= '#forge:storage_blocks/raw_' + oreName;
+	data.ingot 				= '#forge:ingots/' + oreName;
+	data.crushed			= 'create:crushed_raw_' + oreName;
+	data.nugget				= '#forge:nuggets/' + oreName;
+	data.byproduct			= '';
+	data.moltenFluid		= '';
+	data.moltenByproduct	= [];
+	//data.dust = 'alltheores:' + oreName + '_dust';
+	
+	//------------------------------------------------
+	// Setup
+	//------------------------------------------------
+	function setup(event) {
+		removeRecipes(event);
+		addRecipes(event);
+	}
+	
+	//------------------------------------------------
+	// Remove Recipes
+	//------------------------------------------------
+	function removeRecipes(event) {
+		// If item is Metal, and Using ReducedSmelting
+		if (isMetal && Fox.Processing.UsingReducedSmelting) {
+			// Remove Smelting
+			namespace.Smelting.RemoveRecipeByInput(event, data.raw);
+			// Remove Blasting
+			namespace.Blasting.RemoveRecipeByInput(event, data.raw);
+			// Millstone
+			namespace.Millstone.RemoveRecipeByInput(event, data.raw);
+		}
+
+		// Remove Crushing - Ore
+		namespace.Crushing.RemoveRecipeByInput(event, data.ore);
+		// Remove Crushing - Raw
+		namespace.Crushing.RemoveRecipeByInput(event, data.raw);
+		// Remove Crushing - Raw Block
+		namespace.Crushing.RemoveRecipeByInput(event, data.rawBlock);
+		
+		// Remove Washing
+		namespace.Washing.RemoveRecipeByInput(event, data.crushed);
+		
+		// Remove Melting
+		//namespace.Melting.RemoveOreMeltingRecipeByInput(event, data.raw);
+		//namespace.Melting.RemoveOreMeltingRecipeByInput(event, data.crushed);
+		//namespace.Melting.RemoveOreMeltingRecipeByInput(event, data.ore);
+
+		// Remove Molten
+		//---
+	}
+
+	//------------------------------------------------
+	// Add Recipes
+	//------------------------------------------------
+	function addRecipes(event) {
+		// If item is Metal, and Using ReducedSmelting
+		if (isMetal && Fox.Processing.UsingReducedSmelting) {
+			// Add Smelting
+			namespace.Crushing.AddRecipe(event, data.raw, data.nugget, namespace.GetSmeltingAmount());
+			// Add Blasting
+			namespace.Crushing.AddRecipe(event, data.raw, data.nugget, namespace.GetBlastingAmount());
+			// Millstone
+			namespace.Millstone.AddRecipe(event, data.raw, data.crushed, 1);
+		}
+		
+		// Add Crushing - Ore
+		namespace.Crushing.AddRecipe(event, data.raw, data.crushed, namespace.CrushingAmount * breakAmount, data.byproduct, breakAmount, namespace.CrushingGivesNuggets, breakAmount);
+		// Add Crushing - Raw
+		namespace.Crushing.AddRecipe(event, data.raw, data.crushed, namespace.CrushingAmount, data.byproduct, 1, namespace.CrushingGivesNuggets, 1);
+		// Add Crushing - Raw Block
+		namespace.Crushing.AddRecipe(event, data.raw, data.crushed, namespace.CrushingAmount * 9, data.byproduct, 9, namespace.CrushingGivesNuggets, 9);
+
+		// Add Washing
+		namespace.Washing.AddRecipe(event, data.crushed, data.nugget, namespace.WashingAmount, data.byproduct, 1, namespace.WashingGivesNuggets, 1)
+		
+		// Add Melting
+		//namespace.Melting.AddCrushedOreRecipe(event, data.crushed, data.moltenFluid, namespace.MeltingRawToFluidAmount, data.moltenByproduct, Fox.Processing.MeltingRawToByproductAmount, namespace.MeltingTempCoal);
+
+		// Add Molten
+		//---
+	}
+
+	// Add Setup to OresSetup List
+	if (oreName != '') {
+		namespace.OresSetup.push(setup);
+	}
+
+}());
