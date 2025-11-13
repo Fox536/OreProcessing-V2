91fa79fb96296c08a4539fdd2dca4500b5288ec8
fox536 <ultimate_x_25@yahoo.com>
2025-11-13 13:25:44 -0600

    Rearranged files

--- ATM10/Changes/tags/industrialSoul_productiveBees.js
+++
@@ -1,6 +0,0 @@
-// DISCLAIMER: This code is provided as-is with no guarantees. It has been created // for a specific use case and may not work in all environments.
-// Use at your own risk. Always back up your files/worlds before making changes! I // cannot be held accountable for any unintended consequences of using this code.
-
-ServerEvents.tags('block', event => {
-	event.removeAll('productivebees:untickable')
-})
--- ATM10/Changes/tags/timeInABottle.js
+++
@@ -1,4 +0,0 @@
-ServerEvents.tags('block', event => {
-  // Modular Bees
-  event.remove('tiab:un_acceleratable', 'modularbees:modular_beehive_core');
-})
