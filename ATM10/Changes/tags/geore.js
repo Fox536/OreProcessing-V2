9bd4a210de5dbc80992d7fcb5a90740b3dfe4e53
Fox536 <34255581+Fox536@users.noreply.github.com>
2025-11-13 13:17:12 -0600

    Add files via upload

---
+++ ATM10/Changes/tags/geore.js
@@ -0,0 +1,20 @@
+ServerEvents.tags("block", (allthemods) => {
+	allthemods.remove("c:relocation_not_supported", [
+		"geore:budding_diamond",
+		"geore:budding_gold",
+		"geore:budding_iron",
+		"geore:budding_copper",
+		"geore:budding_redstone",
+		"geore:budding_lapis",
+		"geore:budding_coal",
+		"geore:budding_quartz",
+		"geore:budding_ruby",
+		"geore:budding_emerald",
+		"geore:budding_sapphire",
+		"geore:budding_zinc",
+		"geore:budding_topaz",
+		"geore:budding_allthemodium",
+		"geore:budding_vibranium",
+		"geore:budding_unobtainium"
+	])
+});
---
+++ ATM10/Changes/tags/timeInABottle.js
@@ -0,0 +1,4 @@
+ServerEvents.tags('block', event => {
+  // Modular Bees
+  event.remove('tiab:un_acceleratable', 'modularbees:modular_beehive_core');
+})
